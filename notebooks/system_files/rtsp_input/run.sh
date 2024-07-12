252fd6fc1538a33bb05733ad2f816fc6fc66971a
Martin Rios <mrios90@gmail.com>
2024-07-11 23:33:02 -0600

    Delete notebooks/system_files directory

--- notebooks/system_files/rtsp_input/run.sh
+++
@@ -1,7 +0,0 @@
-docker build -t video-sampler . && \  # comentar linea si ya esta la imagen creada
-docker run --env-file .env \
--v ./videos:/app/videos \
-video-sampler \
-rtsp://soft:FTP2023sof@45.171.132.165:554/h264/ch1/main/av_stream \  # video source. If file, it should start with "./videos/"...
-2 \  # n of frames per second
-ukw-wildfire-camera001-samples # s3 bucket name
